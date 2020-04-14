eeaaa31af43bc9e5768de043176d860f0004455c
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-04-14 18:14:19 +0400

    Wrote Testing Script

--- test.sh
+++ test.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-EXEC="test_exec"
+
 PROG="gamma"
-REAL_PROG="$(realpath "$PROG")"
-DIR="$2"
+DIR="$1"
 RED='\033[1;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-FILES="${DIR}/*.in"
+#FILES="${DIR}/*.c"
+FILES=$(find $directory -type f -name "*.in")
+make gamma
+
 for f in $FILES; do
-  out="${f::-2}"
-  out="${out}out"
-
-  err="${f::-2}"
-  err="${err}err"
-  valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q "$REAL_PROG" -g <"$f" >"$EXEC".out 2>"$EXEC".err
-
-  DIFF1="$(diff "$out" "$EXEC".out)"
-  DIFF2="$(diff "$err" "$EXEC".err)"
-  if [ "$DIFF1" ] || [ "$DIFF2" ]; then
-    echo -e "${RED} ${f} didn't pass ${NC}"
+  base="$(basename ${f})"
+  name="${base::-2}"
+  echo $name
+  cp ${f} ./src
+  gcc -Wall -Wextra -std=c11 -O2 -c "src/${base}"
+  gcc  -o gamma "${name}.o" gamma.o safe_malloc.o ufind.o
+  valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q ./gamma -g
+  if [ $? -eq 0 ]; then
+    echo -e "${GREEN} ${name} passed${NC}"
   else
-    echo -e "${GREEN} ${f} passed${NC}"
+    echo -e "${RED} ${name} didn't pass ${NC}"
   fi
+  rm -f "${name}.o" ${base}
 done
-rm "$EXEC".out
-rm "$EXEC".err
+rm gamma *.o vgcore.*

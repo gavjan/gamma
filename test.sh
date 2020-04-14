4f83e851ee43bd80cd351e92333062a6a8661731
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-04-14 18:22:25 +0400

    test script fix #2

--- test.sh
+++ test.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-PROG="gamma"
 DIR="$1"
 RED='\033[1;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 #FILES="${DIR}/*.c"
-FILES=$(find $directory -type f -name "*.in")
+FILES=$(find "$DIR" -type f -name "*.c")
 make gamma
 
 for f in $FILES; do
-  base="$(basename ${f})"
+  base=$(basename "${f}")
   name="${base::-2}"
-  echo $name
-  cp ${f} ./src
+  cp "${f}" ./src
   gcc -Wall -Wextra -std=c11 -O2 -c "src/${base}"
   gcc  -o gamma "${name}.o" gamma.o safe_malloc.o ufind.o
   valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q ./gamma -g

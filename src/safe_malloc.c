a800dc8c26a6b131971d0dc16e23804e4ff0bc55
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-04-22 11:42:16 +0400

    thinking of future

--- src/safe_malloc.c
+++ src/safe_malloc.c
@@ -5,7 +5,11 @@
  */
 #include "safe_malloc.h"
 #include <stdlib.h>
+#include <string.h>
 void* safe_free(void* p) {
 	if(p) free(p);
 	return NULL;
 }
+//int safe_strcmp(const char *s1, const char *s2) {
+//	return strncmp(s1,s2,strlen(s2));
+//}

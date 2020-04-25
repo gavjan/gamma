f7a433b5cbd0b085157188f63705a372cfe36273
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-04-25 18:18:22 +0400

    redundancy removed

--- src/main.c
+++ src/main.c
@@ -1,42 +1,16 @@
-// [IPP] Małe Zadanie(Small Task), Gevorg Chobanyan 401929
-#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include "safe_malloc.h"
+#include <stdint.h>
 #include "parser.h"
 #include "gamma.h"
 #include "handler.h"
-#include <ctype.h>
-// Clear tokens and reset strings
-static inline Command clear_tokens(Command command) {
-	command.player=safe_realloc(command.player, sizeof(char));
-	command.tree=safe_realloc(command.tree, sizeof(char));
-	command.animal=safe_realloc(command.animal, sizeof(char));
-	command.player[0]='\0';
-	command.tree[0]='\0';
-	command.animal[0]='\0';
-	*command.forest_capacity=STARTING_SIZE;
-	*command.tree_capacity=STARTING_SIZE;
-	*command.animal_capacity=STARTING_SIZE;
-	return command;
-}
 
-// Check if line contains white spaces
-static inline bool isspace_line(const char* line) {
-	int i=0;
-	while(line[i]) {
-		if(!isspace(line[i])) return false;
-		i++;
-	}
-	return true;
-}
+
 int main() {
 	gamma_t* g=NULL;
 	Command command;
 	int first_char;
-	u_int64_t line_num=0;
+	uint64_t line_num=0;
 	while((first_char=fpeek())!=EOF) {
 		line_num++;
 		if(first_char == '\n' || first_char == '#')

c85529dd344bbc023cfc7ce82ddfe0fa7c80b9ed
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-05-04 14:53:46 +0400

    testing script progress bar

--- test_part2.sh
+++ test_part2.sh
@@ -6,13 +6,23 @@ function ctrl_c() {
   #rm -rf test_build
 
   printf "\n\n--\n number of tests that didn't pass: "
-  if [ "$counter" -eq 0 ]; then
+  if [ "$err_counter" -eq 0 ]; then
     echo -e "${GREEN}0"
   else
-    echo -e "${RED} $counter"
+    echo -e "${RED} $err_counter"
   fi
   exit 0
 }
+
+function progress_bar {
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+printf "\r[${_fill// /#}${_empty// /-}] ${_progress}%%"
+}
+
 
 if [ ! -d "test_build" ] ; then
     # shellcheck disable=SC2164
@@ -31,22 +41,34 @@ RED='\033[1;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-counter=0
+err_counter=0
+progress_count=0
+progress_max="$(find ${DIR}/*.in | wc -l)"
+
+progress_bar ${progress_count} ${progress_max}
 FILES="${DIR}/*.in"
 for f in $FILES; do
+
+  progress_count=$((progress_count+1))
   out="${f::-2}"
   out="${out}out"
   err="${f::-2}"
   err="${err}err"
 
   valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q "$REAL_PROG" <"$f" >"$EXEC".out 2>"$EXEC".err
+
+  # Reset progress bar
+  printf "\r                                                \r"
+
   DIFF1="$(diff "$out" "$EXEC".out)"
   DIFF2="$(diff "$err" "$EXEC".err)"
   if [ "$DIFF1" ] || [ "$DIFF2" ]; then
-    counter=$((counter+1))
+    err_counter=$((err_counter+1))
     echo -e "${RED} ${f} didn't pass ${NC}"
   else
     echo -e "${GREEN} ${f} passed${NC}"
   fi
+
+  progress_bar ${progress_count} ${progress_max}
 done
 ctrl_c
